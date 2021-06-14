47c4cc05489c472b90fd34ffe40510749911cef4
yangyuchen <2504477167@qq.com>
2021-06-14 10:40:04 +0800

    Initial commit

---
+++ src/main/java/com/hwj/tgy/App.java
@@ -0,0 +1,13 @@
+package com.hwj.tgy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
