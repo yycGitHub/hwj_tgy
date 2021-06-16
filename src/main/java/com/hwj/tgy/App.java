3462da0e05f33fc2af57944232f23dadcef20414
yangyuchen <2504477167@qq.com>
2021-06-16 09:33:46 +0800

    20210616

--- src/main/java/com/hwj/tgy/App.java
+++ src/main/java/com/hwj/tgy/App.java
@@ -1,9 +1,14 @@
 package com.hwj.tgy;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@RestController
+@EnableAutoConfiguration
 public class App 
 {
     public static void main( String[] args )
