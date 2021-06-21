3f551e6b3451cb96741dad46fe649553310f3c8f
yangyuchen <2504477167@qq.com>
2021-06-21 10:11:56 +0800

    20210616

--- src/main/java/com/hwj/tgy/App.java
+++ src/main/java/com/hwj/tgy/App.java
@@ -1,13 +1,19 @@
 package com.hwj.tgy;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@MapperScan("com.hwj.tgy.dao")
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
